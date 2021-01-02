a0338498cb6464e52282721ef4fc009c01821917
BlaD200 <blad600@gmail.com>
2021-01-02 18:46:51 +0200

    Configured startup
    Signed-off-by: BlaD200 <blad600@gmail.com>

--- app.py
+++ app.py
@@ -1,12 +1,52 @@
-from flask import Flask
+import json
+import logging
+from urllib.request import urlopen
+
+import telegram
+from flask import Flask, request
+from telegram.ext import Dispatcher
+
+from bot.config import WEBHOOK_URL, BOT_TOKEN
+from bot.setup_bot import setup
+
 
 app = Flask(__name__)
 
+# Declaring a global dispatcher
+dispatcher: Dispatcher
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+# Registering logger here
+logging.basicConfig(format='%(levelname)s %(name)s | %(asctime)s | %(message)s',
+                    level=logging.INFO)
+logger: logging.Logger = logging.getLogger(__name__)
+
+
+@app.route("/", methods=["GET", "HEAD"])
+def index():
+    return '<h1>Telegram Bot by <a href="tg://user?id=386151408">Vlad Synytsyn</a></h1>'
+
+
+@app.route('/', methods=['Post'])
+def webhook():
+    json_request = request.get_json()
+    update = telegram.Update.de_json(json_request, dispatcher.bot)
+    dispatcher.process_update(update)
+    return json.dumps({'success': True}), 200, {'ContentType': 'application/json'}
 
 
 if __name__ == '__main__':
+    # Check, if bot correctly connect to Telegram API
+    bot = telegram.Bot(BOT_TOKEN)
+    info = bot.get_me()
+    logger.info(f'Bot info: {info}')
+    logger.info(bot.get_webhook_info())
+
+    if bot.get_webhook_info()['url'] != WEBHOOK_URL:
+        urlopen(f'https://api.telegram.org/bot{BOT_TOKEN}/setWebhook?url={WEBHOOK_URL}')
+
+    logger.info(bot.get_webhook_info())
+
+    dispatcher, _ = setup()
+    logging.info('Started server with webhook')
+    app.debug = True
     app.run()
---
+++ bot/setup_bot.py
@@ -0,0 +1,48 @@
+import logging
+
+from telegram import Update, ParseMode
+from telegram.ext import Updater, CallbackContext, MessageHandler, Filters
+
+from bot.config import BOT_TOKEN
+
+
+# Registering logger here
+logging.basicConfig(format='%(levelname)s %(name)s | %(asctime)s | %(message)s',
+                    level=logging.INFO)
+logger: logging.Logger = logging.getLogger(__name__)
+
+
+def setup():
+    logging.info("Setup bot")
+    updater = Updater(BOT_TOKEN, use_context=True)
+    dispatcher = updater.dispatcher
+
+    # Registering commands handlers here #
+
+    # Registering conversation handlers here
+
+    # Registering handlers here #
+    start_message_handler = MessageHandler(Filters.all, start_handler)
+    dispatcher.add_handler(start_message_handler)
+
+    return dispatcher, updater
+
+
+def start_handler(update: Update, context: CallbackContext):
+    update.message.reply_text(
+        text=f"Hello, [{update.message.from_user.full_name}](tg://user?id={update.message.from_user.id})!",
+        parse_mode=ParseMode.MARKDOWN
+    )
+
+
+def test_server():
+    logging.info('Started server with polling')
+    # Do NOT USE it in a production deployment.
+    # for PRODUCTION use WEBHOOK
+    _, updater = setup()
+    updater.start_polling()
+    updater.idle()
+
+
+if __name__ == '__main__':
+    test_server()
