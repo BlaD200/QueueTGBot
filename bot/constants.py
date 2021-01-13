c915683c9d9deea0481b0f5452f084a3251bae86
BlaD200 <blad600@gmail.com>
2021-01-13 20:38:08 +0200

    specified the bot version directly in the code
    Signed-off-by: BlaD200 <blad600@gmail.com>

--- bot/constants.py
+++ bot/constants.py
@@ -1,10 +1,11 @@
 from os import getenv
 
 
+BOT_VERSION = 'unreleased'
+
 BOT_TOKEN = getenv('BOT_TOKEN')
 WEBHOOK_URL = getenv('WEBHOOK_URL')
 ADMIN_ID = getenv('ADMIN_ID')
-BOT_VERSION = getenv('BOT_VERSION')
 
 __all__ = [
     'BOT_TOKEN',
