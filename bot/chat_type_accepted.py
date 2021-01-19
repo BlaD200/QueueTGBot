bf400c859032414e7812ae6b6ba190116f312a0e
BlaD200 <blad600@gmail.com>
2021-01-19 13:41:43 +0200

    Added private_only_handler for handlers, used in private chats.
    Signed-off-by: BlaD200 <blad600@gmail.com>

--- bot/chat_type_accepted.py
+++ bot/chat_type_accepted.py
@@ -5,7 +5,11 @@ from typing import Callable, Any
 from telegram import Update
 from telegram.ext import CallbackContext
 
+from app_logging import get_logger
 from localization.replies import private_unaccepted
+
+
+logger = get_logger(__name__)
 
 
 def group_only_command(handler: Callable[[Update, CallbackContext], Any]):
@@ -31,6 +35,33 @@ def group_only_command(handler: Callable[[Update, CallbackContext], Any]):
     return group_only_command_wrapper
 
 
+def private_only_handler(handler: Callable[[Update, CallbackContext], Any]):
+    """
+    Decorator function.
+
+    The decorated function will be called ONLY if the chat type is 'private', otherwise, the call will be omitted.
+
+    Note:
+        It is used to decorate handlers, that HAVE TO accept two arguments:
+        :class:`telegram.Update` and :class:`telegram.CallbackContext`
+
+        You can also use ``Filters.chat_type.private`` for filtering your messages.
+
+    Args:
+        handler: handler function
+
+    Returns:
+        given function with the chat type check
+    """
+
+    def private_only_handler_wrapper(update: Update, context: CallbackContext):
+        if update.effective_chat.type == 'private':
+            return handler(update, context)
+
+    return private_only_handler_wrapper
+
+
 __all__ = [
-    'group_only_command'
+    'group_only_command',
+    'private_only_handler'
 ]
